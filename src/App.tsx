490d930717e23fc8be0eeb8d09c51d111240dfab
Inna <kostiuk.inna@gmail.com>
2019-12-20 17:23:05 +0800

    add modules to create event, try to register event on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import './App.css';
 
 const App: React.FC = () => {
+  // const calendarComponentRef = React.createRef<FullCalendar>()
+  const [calendarEvents, setcalendarEvents] = useState([ { title: 'Event Now', start: new Date() }]);
+
   return (
     <FullCalendar 
-      defaultView="dayGridMonth" 
-      plugins={[dayGridPlugin]}
+      defaultView="dayGridMonth"
+      header={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      }}
+      plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+      dateClick={ (arg) => handleClick(arg, calendarEvents, setcalendarEvents) }
     />
   );
+  
+}
+
+function handleClick(arg: any, calendarEvents: any, setcalendarEvents: any) {
+  setcalendarEvents({ 
+    calendarEvents: calendarEvents.concat({
+      title: 'New Event',
+      start: arg.date,
+      allDay: arg.allDay
+    })
+  })
 }
 
 export default App;
