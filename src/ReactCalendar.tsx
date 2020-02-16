6321a2a802d0bffae64bffe744bb6b2f5e6a9350
Inna <kostiuk.inna@gmail.com>
2020-02-17 08:42:11 +0900

    save progress, can add event to calendar

--- src/ReactCalendar.tsx
+++ src/ReactCalendar.tsx
@@ -4,33 +4,55 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import bootstrapPlugin from '@fullcalendar/bootstrap';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+function addOneHour(currentTime: Date) {
+    return new Date(currentTime.setHours(currentTime.getHours() + 1));
+}
 
 export const ReactCalendar: React.FC = () => {
 
+    const calendarRef = React.createRef<FullCalendar>();
+
     //TODO: maybe add typing for useState?
-    let today = new Date().toISOString();
-    const [calendarEvents, setCalendarEvents] = useState([ { title: 'Current Event Title', start: today, description: 'Work' } ])
+    let today = new Date();
+    let eventStart = today.toISOString();
+    let eventEnd = addOneHour(today).toISOString();
     const [modal, setModal] = useState(false);
+    const [isNewEvent, setIsNewEvent] = useState(false);
+    const [eventId, setEventId] = useState(0);
     const [formTitle, setFormTitle] = useState('');
     const [formDescription, setFormDescription] = useState('');
     const [formDate, setFormDate] = useState('');
     const [formTimeStart, setFormTimeStart] = useState('');
     const [formTimeEnd, setFormTimeEnd] = useState('');
+    const [calendarEvents, setCalendarEvents] = useState([
+        {   
+            id: eventId,
+            title: 'Current Event Title',
+            start: eventStart,
+            end: eventEnd,
+            description: 'Work' //non-standart field
+        }
+    ]);
 
     const toggle = () => setModal(!modal);
 
     const handleDateClick = (event: any) => {
         //do check here if event has time slot clicked, 
         //if yes set time from event, if no set timeSrart & timeEnd to current time
-        toggle();
         setFormDate(event.dateStr);
         let timeNow = new Date();
         let start = timeNow.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
-        let timePlusOneHour = new Date(timeNow.setHours(timeNow.getHours() + 1));
+        let timePlusOneHour = addOneHour(timeNow);
         let end = timePlusOneHour.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+        setEventId(eventId + 1);
+        setFormTitle('');
         setFormTimeStart(start);
         setFormTimeEnd(end);
+        setFormDescription('');
+        setIsNewEvent(true);
+        toggle();
     }
 
     const handleInputChange = (event: any) => {
@@ -62,18 +84,62 @@ export const ReactCalendar: React.FC = () => {
 
     const handleSubmit = (event: any) => {
         toggle();
+        // create new Id
         //create event start time
         const eventStart = formDate + "T" + formTimeStart + ":00";
+        const eventEnd = formDate + "T" + formTimeEnd + ":00";
         //add new event to events list
-        setCalendarEvents(calendarEvents.concat( { title: formTitle, start: eventStart, description: formDescription} ));
+        setCalendarEvents(calendarEvents.concat({
+            id: eventId,
+            title: formTitle,
+            start: eventStart,
+            end: eventEnd,
+            description: formDescription
+         }));
+        // calendarRef.current?.getApi().addEvent({
+        //     id: eventId,
+        //     title: formTitle,
+        //     start: eventStart,
+        //     end: eventEnd,
+        //     description: formDescription
+        // })
         setFormTitle('');
         setFormDescription('');
         event.preventDefault();
       }
 
+      const handleEventClick = (info: any) => {
+          setIsNewEvent(false);
+          toggle();
+          let selectedEvent = info.event;
+          console.log(`selectedEvent: `, selectedEvent.start.toISOString());
+          let selectedEventTimeStart = selectedEvent.start.toISOString().split('T')[1].substring(0,5);
+          let selectedEventTimeEnd = selectedEvent.end.toISOString().split('T')[1].substring(0,5);
+          setFormDate(selectedEvent.start.toISOString().split('T')[0]);
+          console.log(`selectedEventTimeStart: `, selectedEventTimeStart);
+          setFormTimeStart(selectedEventTimeStart);
+          setFormTimeEnd(selectedEventTimeEnd);
+          setFormTitle(selectedEvent.title);
+          setFormDescription(selectedEvent.extendedProps.description);
+      }
+
+      const handleEventDelete = (e: any) => {
+        // let calendarApi = calendarRef.current?.getApi();
+        let deleteEventId = e.target.getAttribute("id");
+        //removes from current calendar
+        // let deleteEvent = calendarApi?.getEventById(deleteEventId);
+        console.log(`events before: `, calendarEvents);
+        //this doesn't work
+        setCalendarEvents(calendarEvents.filter((event) => event.id !== deleteEventId ));
+        // deleteEvent?.remove();
+        console.log(`events after: `, calendarEvents);
+        toggle();
+      }
+
     return (
         <div>
             <FullCalendar
+                ref={calendarRef}
                 defaultView="dayGridMonth"
                 themeSystem="bootstrap"
                 header={{
@@ -84,9 +150,10 @@ export const ReactCalendar: React.FC = () => {
                 plugins={ [ dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrapPlugin ] }
                 events={ calendarEvents }
                 dateClick={ handleDateClick }
+                eventClick = { handleEventClick }
             />
             <Modal isOpen={modal} toggle={toggle}>
-                <ModalHeader toggle={toggle}>Add Event</ModalHeader>
+                <ModalHeader toggle={toggle}>{isNewEvent ? 'Add Event' : 'Edit Event'}</ModalHeader>
                 <ModalBody>
                     <form onSubmit={handleSubmit}>
                         <label>Date:</label><br />
@@ -122,11 +189,10 @@ export const ReactCalendar: React.FC = () => {
                             value={formDescription}
                             onChange={handleInputChange} />
                         <hr />
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Save" />
+                        {!isNewEvent ? <input type="button" value="Delete" id={eventId.toString()} onClick={ handleEventDelete } /> : null }
                     </form>
                 </ModalBody>
-                <ModalFooter>
-                </ModalFooter>
             </Modal>
         </div>
     );
